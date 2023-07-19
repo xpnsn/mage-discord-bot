7ad8c9c555d7cc07b654e51032936e458a37fba8
himanshu <kumarehimanshu1@gmail.com>
2023-07-19 22:30:35 +0530

    ok

--- commands/reaction-role/button-roles.js
+++ commands/reaction-role/button-roles.js
@@ -4,7 +4,10 @@ module.exports = {
     data: new SlashCommandBuilder()
     .setName('reaction-role')
     .setDescription('Add role pair with emoji or text through this ID')
-    .addRoleOption(option => option.setName('role').setDescription('Name of the role').required(true)),
+    .addRoleOption(option => option.setName('role-1').setDescription('Select the role').setRequired(true))
+    .addStringOption(option => option.setName('button-name-1').setDescription('Add name of the role displays on the button').setRequired(true))
+    .addRoleOption(option => option.setName('role-2').setDescription('Select the role'))
+    .addStringOption(option => option.setName('button-name-2').setDescription('Add name of the role displays on the button')),
     
 
     async execute(interaction) {
